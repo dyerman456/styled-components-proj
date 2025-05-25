47116cfed481eb8ce390c9b51376f93f054c9eea
dyerman456 <dyerman456@gmail.com>
2025-05-25 21:52:22 +0200

    add Menu and Box components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Menu>
+                <ul>
+                    <li><a href="">menu item 1</a></li>
+                    <li><a href="">menu item 2</a></li>
+                    <li><a href="">menu item 3</a></li>
+                </ul>
+            </Menu>
+            <Box>
+                <StyledBtn as={Link} href="#">LinkComponent</StyledBtn>
+                <StyledBtn as="a" href="#">Link</StyledBtn>
+                <StyledBtn>Hello</StyledBtn>
+                <SuperButton>Super Button</SuperButton>
+            </Box>
+        </div>
+    );
 }
 
 export default App;
+
+const StyledBtn = styled.button`
+    border: none;
+    background-color: #ff8cb4;
+    padding: 10px 20px;
+    color: snow;
+    font-size: 2rem;
+    font-weight: bold;
+
+    &:hover {
+        background-color: #2a50cd;
+    }
+
+    &:last-child {
+        background-color: #94f874;
+    }
+`
+
+const Link = styled.a`
+    color: #5997fd;
+    font-size: 2rem;
+    font-weight: bold;
+    background-color: transparent;
+    padding: 0;
+`
+
+const SuperButton = styled(StyledBtn)`
+    border-radius: 5px;
+    background-color: #ffebab;
+    color: #616161;
+`
+
+const Box = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    
+    button {
+        cursor: pointer;
+    }
+
+    ${Link} {
+        cursor: zoom-in;
+    }
+    
+`
+
+const Menu = styled.nav`
+
+`
